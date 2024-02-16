0df431f3a255b70ce1bfdfbf3269d78b9875e9c2
Aish Mishra <aishlunatic001@gmail.com>
2024-02-16 20:09:48 +0530

    Jwt assignment done

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
+const zod = require("zod"); 
 
 
 /**
@@ -13,9 +14,25 @@ const jwtPassword = 'secret';
  *                        Returns null if the username is not a valid email or
  *                        the password does not meet the length requirement.
  */
+
+const emailSchema = zod.string().email(); 
+const passwordSchema = zod.string().min(6);
 function signJwt(username, password) {
-    // Your code here
+
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password) ; 
+   
+    if(!usernameResponse.success || !passwordResponse.success ){
+        return null ; 
+    }
+
+    const signature = jwt.sign({
+        username
+    }, jwtPassword )
+
+    return signature ; 
 }
+
 
 /**
  * Verifies a JWT using a secret key.
@@ -26,7 +43,17 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+    try{
+       jwt.verify(token,jwtPassword); 
+        return true ; 
+
+    }
+    catch(e){
+       
+            return false ; 
+        
+
+    }
 }
 
 /**
@@ -37,7 +64,14 @@ function verifyJwt(token) {
  *                         Returns false if the token is not a valid JWT format.
  */
 function decodeJwt(token) {
-    // Your code here
+    // true or false 
+    const decoded = jwt.decode(token) ; 
+    if(decoded){
+        return true ; 
+    }
+    else{
+        return false ; 
+    }
 }
 
 
